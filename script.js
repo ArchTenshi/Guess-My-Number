096395019a73375681ec6b63e76cb7ed2f97ef1a
Dmytro Cherednichenko <80962356+ArchTenshi@users.noreply.github.com>
2021-03-21 11:19:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@
 // document.querySelector('.number').textContent = 5;
 // document.querySelector('.guess').value = 5;
 let number = Math.ceil(Math.random() * 20),
-  s = 20,
-  hs = 0,
+  score = 20,
+  highscore = 0,
   text = '';
 console.log(number);
 document.querySelector('.check').addEventListener('click', function () {
@@ -14,31 +14,32 @@ document.querySelector('.check').addEventListener('click', function () {
     text = 'Input Number!';
   } else if (inputNumber == number) {
     text = 'That is correct number!';
-    hs < s ? (hs = s) : (hs = hs);
+    highscore < score ? (highscore = score) : (highscore = highscore);
     document.querySelector('.number').textContent = inputNumber;
     document.body.style.background = '#00b200';
     document.querySelector('.number').style.width = '30rem';
-  } else if (s > 1) {
+  } else if (score > 1) {
     if (inputNumber < number) {
       text = 'Too low';
-      s--;
+      score--;
     } else {
       text = 'Too high';
-      s--;
+      score--;
     }
   } else {
     text = 'You lost!';
-    s = 0;
+    score = 0;
   }
-  document.querySelector('.score').textContent = s;
-  document.querySelector('.highscore').textContent = hs;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.highscore').textContent = highscore;
   document.querySelector('.message').textContent = text;
 });
 document.querySelector('.again').addEventListener('click', function () {
-  s = 20;
+  score = 20;
   number = Math.floor(Math.random() * 19 + 1) + 1;
   document.querySelector('.guess').value = '';
   document.querySelector('.number').textContent = '?';
   document.body.style.background = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
+
